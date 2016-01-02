15a31e4fd39396becbae4f76246e1029b84e4df2
Reto Gmür <reto@apache.org>
2016-01-02 19:36:58 +0100

    removed obsolete startup script

--- startup.sh
+++
@@ -1,8 +0,0 @@
-docker run -ti --rm \
-            -e DISPLAY=$DISPLAY \
-           -v /tmp/.X11-unix:/tmp/.X11-unix \
-           -v `pwd`/.m2:/home/developer/.m2 \
-           -v `pwd`/.ivy2:/home/developer/.ivy2 \
-           -v `pwd`/config:/home/developer/config  \
-           -v `pwd`/projects:/home/developer/NetBeansProjects \
-           reto/netbeans
